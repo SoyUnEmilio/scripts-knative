dabc0ec86d484b5fd8a8d4c38a564d5bb25eb7be
SoyUnEmilio <milio81@gmail.com>
2019-05-23 20:48:30 +0200

    change default minikube home

--- 0_setenv.sh
+++ 0_setenv.sh
@@ -2,7 +2,7 @@
 
 echo 'source 0_setenv.sh'
 
-export MINIKUBE_HOME=/Users/burrsutter/minikube_1.0.0/bin;
+export MINIKUBE_HOME=~/minikube_1.0.0/bin;
 export PATH=$MINIKUBE_HOME:$PATH
 # export KUBECONFIG=$MINIKUBE_HOME/.kube/config:$MINIKUBE_HOME/.kube/kubconfig2
 export KUBECONFIG=$MINIKUBE_HOME/.kube/config
